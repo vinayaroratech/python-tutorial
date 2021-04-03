a96eb4c5cafb75c8593a08da9d3016c203c68024
Vinay Arora <vinay.arora7171@hotmail.com>
2021-04-03 19:39:13 +0530

    Python syntax Indentation & comments

---
+++ syntax.py
@@ -0,0 +1,23 @@
+# Indentation: refers to the spaces at the beginning of a code line.
+"""
+The number of spaces is up to you as a programmer, but it has to be at least one.
+"""
+
+x = 5
+y = 2
+
+if x > y:
+    print("Using Tab Indentation")
+if x > y:
+    print("Using Single Space Indentation")
+if x > y:
+    print("Using Multiple Spaces Indentation")
+
+# Comments: starts with a #, and Python will ignore them:
+""" 
+Comments can be placed at the end of a line, and Python will ignore the rest of the line:
+Multi Line Comments: Python does not really have a syntax for multi line comments.
+To add a multiline comment you could insert a # for each line:
+Since Python will ignore string literals that are not assigned to a variable, 
+you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+"""
